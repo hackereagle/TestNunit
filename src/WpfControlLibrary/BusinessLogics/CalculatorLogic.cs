b012f08f3f78fe10c4c8e0ba254ba3d1fac51024
Bill <sclin@brightestTECH.com>
2023-02-23 17:36:10 +0800

    Chg: Declared view model properties and binding to view. New: Created business logic class.

---
+++ src/WpfControlLibrary/BusinessLogics/CalculatorLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlLibrary.BusinessLogics
+{
+    internal class CalculatorLogic
+    {
+        private ISubject<string> mCurExpression;
+        public IObservable<string> CurrentExpression => mCurExpression.AsObservable();
+        private ISubject<string> mCurVal;
+        public IObservable<string> Value => mCurVal.AsObservable();
+
+
+    }
+}
---
+++ src/WpfControlLibrary/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfControlLibrary.Base;
+
+namespace WpfControlLibrary.ViewModel
+{
+    internal class CalculatorViewModel : Base.ViewModelBase
+    {
+        BusinessLogics.CalculatorLogic mCalculatorLogic;
+        public CalculatorViewModel()
+        { 
+            this.mCalculatorLogic = new BusinessLogics.CalculatorLogic();
+        }
+
+        private string mCurrentExpression;
+        public string CurrentExpression
+        {
+            get { return mCurrentExpression; } 
+            set
+            {
+                mCurrentExpression = value;
+                OnPropertyChanged("CurrentExpression");
+            }
+        }
+
+        private string mValue;
+        public string Value
+        {
+            get { return mValue; }
+            set
+            { 
+                mValue = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        AsyncCommand<object> mNumsCommand;
+    }
+}
